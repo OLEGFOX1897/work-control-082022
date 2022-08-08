15697d071820f2d4d0d71665de1b96681395b3bb
oleg1 <oleglapshin1987@gmail.com>
2022-08-08 20:34:30 +0400

    control work complete

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Контрольная работа 082022 
+
+string [] arrayStart=new string [] {"columb","-)","turist", "try hard","try", "WWW"}; 
+string [] arrayFinish=new string [arrayStart.Length]; 
+int indTask=0; 
+for (int i = 0; i < arrayStart.Length; i++) 
+{ 
+    if (arrayStart[i].Length<=3) 
+    { 
+        arrayFinish[indTask]=arrayStart[i]; 
+        indTask++; 
+    } 
+} 
+void ShowArray (string [] array) 
+{ 
+    Console.Write("["); 
+    for (int i = 0; i < array.Length; i++) 
+    { 
+        if (array[i]!=null) 
+        { 
+            if (i==0) Console.Write($"''{array[i]}''"); 
+            else Console.Write($", ''{array[i]}''"); 
+        } 
+    } 
+    Console.Write("]"); 
+} 
+ShowArray(arrayStart); 
+Console.Write(" - > "); 
+ShowArray(arrayFinish); 
